265a517376bb84cd227174b4c399ffe45f39b153
Ticean Bennett <ticean@gmail.com>
2011-05-21 13:04:18 -0700

    Enabled some Apache modules.

--- gui-php-app-server-config.sh
+++ gui-php-app-server-config.sh
@@ -28,5 +28,33 @@ apt-get install php5-gd -y
 echo "Enabling mod_rewrite..."
 test -f "/etc/apache2/mods-available/rewrite.load" && ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
 
+
+echo "Enabling Apache modul: headers"
+a2enmod headers
+
+echo "Enabling Apache module: expires"
+a2enmod expires  
+
+echo "Enabling Apache module: deflate"
+a2enmod deflate
+
+echo "Writing configuration file for Apache module: deflate"
+cat > /etc/apache2/mods-available/deflate.conf <<EOF
+<IfModule mod_deflate.c>
+  SetOutputFilter DEFLATE
+  SetEnvIfNoCase Request_URI \.(?:gif|jpe?g|png)$ \
+    no-gzip dont-vary
+  SetEnvIfNoCase Request_URI \
+    \.(?:exe|t?gz|zip|bz2|sit|rar)$ \
+    no-gzip dont-vary
+  SetEnvIfNoCase Request_URI \.pdf$ no-gzip dont-vary
+
+  BrowserMatch ^Mozilla/4 gzip-only-text/html
+  BrowserMatch ^Mozilla/4\.0[678] no-gzip
+  BrowserMatch \bMSIE !no-gzip !gzip-only-text/html
+</IfModule>
+EOF
+
+
 echo "Restarting Apache..."
 /etc/init.d/apache2 restart
